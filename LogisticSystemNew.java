94ff3175bb8cd876426e11d4c63ba90cbc8e8bbf
induwariadhithya <adithya20030615@gmail.com>
2025-10-25 22:02:35 +0530

    Added constants for MAX_CITIES, MAX_DELIVERIES, and FUEL_PRICE

--- LogisticSystemNew.java
+++ LogisticSystemNew.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package logisticsystemnew;
+import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -14,7 +16,13 @@ public class LogisticSystemNew {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    // TODO code application logic here
+    
+    // Commit 2: Define constants and arrays for cities and deliveries
+    static final int MAX_CITIES = 30;
+    static final int MAX_DELIVERIES = 50;
+    static final double FUEL_PRICE = 310.0;
+        
     }
     
 }
